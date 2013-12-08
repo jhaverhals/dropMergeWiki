792cabe36467a6bd126b43aac67c8b3d8fb8d7e2
Anne Stellingwerf <astellingwerf@cordys.com>
2013-12-08 16:01:22 +0100

    Updated RIATransformerProvider to the new format.

--- src/main/java/RIATransformerProvider.groovy
+++ src/main/java/RIATransformerProvider.groovy
@@ -1,33 +1,23 @@
-import com.opentext.dropmerge.*
-import static WarningLevel.High
-import static WarningLevel.Normal
+import com.opentext.dropmerge.dsl.DropMergeInput
 
-public class RIATransformerProvider extends TransformerProvider {
-    private static final BUILDMASTER_HYD = new Jenkins('http://buildmaster-hyd.vanenburg.com/jenkins')
-    private static final CMT_JENKINS = new Jenkins('http://cmt-jenkins.vanenburg.com/jenkins')
+public class RIATransformerProvider {
 
-    private static final JenkinsJob TRUNK_MBV = CMT_JENKINS.withJob('MBV-RIA')
-    private static final JenkinsJob MBV = BUILDMASTER_HYD.withJob('MBV-RIA')
-    private static final JenkinsJob TRUNK_PMD = CMT_JENKINS.withJob('RIA')
-    private static final JenkinsJob PMD = BUILDMASTER_HYD.withJob('RIA-FULLREPORT')
+    public static void main(String[] args) {
+        DropMergeInput.provide {
+            team {
+                name 'RIA'
+            }
 
-    @Override
-    Map<String, Closure<String>> getTransformer(UpdateWikiProperties props) {
-        def transformers = [
-                Team: { item -> CordysWiki.selectOption(item, 'RIA') },
-
-                MBViolationsHighBefore: { TRUNK_MBV.getMBFigure(High) },
-                MBViolationsHighAfter: { MBV.getMBFigure(High) },
-                MBViolationsMediumBefore: { TRUNK_MBV.getMBFigure(Normal) },
-                MBViolationsMediumAfter: { MBV.getMBFigure(Normal) },
-
-                PMDViolationsHighBefore: { TRUNK_PMD.getPMDFigure(High) },
-                PMDViolationsHighAfter: { PMD.getPMDFigure(High) },
-                PMDViolationsMediumBefore: { TRUNK_PMD.getPMDFigure(Normal) },
-                PMDViolationsMediumAfter: { PMD.getPMDFigure(Normal) },
-        ]
-
-        return transformers
+            jenkins {
+                mbv {
+                    wip { job 'MBV-RIA' on buildMasterHYD }
+                    trunk { job 'MBV-RIA' on jenkinsOfCMT }
+                }
+                pmd {
+                    wip { job 'RIA-FULLREPORT' on buildMasterHYD }
+                    trunk { job 'RIA' on jenkinsOfCMT }
+                }
+            }
+        }
     }
-
 }
