3a2a100e356a48b33221c17876d7564e09de9c4c
Anne Stellingwerf <astellingwerf@cordys.com>
2013-12-08 17:59:57 +0100

    Added documentation for ComparingJobsSpec

--- src/main/groovy/com/opentext/dropmerge/dsl/ComparingJobsSpec.groovy
+++ src/main/groovy/com/opentext/dropmerge/dsl/ComparingJobsSpec.groovy
@@ -2,7 +2,31 @@ package com.opentext.dropmerge.dsl
 
 import java.util.regex.Pattern
 
-
+/**
+ * <p>A ComparingJobsSpec specifies a list job, of which some can be compared to each other. For example, one might
+ * specify some jobs that perform unit test. Global jobs could be compared to team's wip jobs.</p>
+ *
+ * <p>The list of jobs is typically specified:
+ * <pre>
+ * withJob { job 'pct-frt-linux' on buildMasterNL; description 'All PCT FRT tests on Linux' }
+ * comparedToJob { job 'PCT-L' on jenkinsOfSVT; description 'All PCT FRT tests on Linux' }
+ * withJob { job 'pct-frt-aix' on buildMasterNL; description 'All PCT FRT tests on AIX' }
+ * </pre></p>
+ *
+ * <p>Not all items in the list are necesarrily comparable. In the example above, the AIX job will not be compared to
+ * any other job. One can specify multiple jobs in a 'withJob' succesively.
+ * None of those are considered comparable. Only when specifying a job with 'comparedToJob', this job will be considered
+ * comparable to the previous job(s). To unite several jobs for comparison, the 'andJob' method can be used:</p>
+ *
+ * <pre>
+ * withJob { job 'pct-frt-junit' on buildMasterNL; description 'All PCT JUnit tests on Windows and Linux' }
+ * andJob { job 'pct-frt-soapunit' on buildMasterNL; description 'All PCT soapunit tests on Windows and Linux' }
+ * andJob { job 'pct-frt-soapui' on buildMasterHYD; description 'All PCT SoapUI tests on Windows and Linux' }
+ * comparedToJob { job 'PCT-L' on jenkinsOfSVT; description 'All PCT FRT tests on Linux' }
+ * andJob { job 'PCT-W' on jenkinsOfSVT; description 'All PCT FRT tests on Windows' }
+ * </pre>
+ *
+ */
 class ComparingJobsSpec extends JobsSpec {
     private Map<JobSpec, JobSpec> comparableJobSpecs = new HashMap<>()
     private Map<JobSpec, DifferencesSpec> justifications = new HashMap<>()
