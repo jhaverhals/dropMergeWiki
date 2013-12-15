4df1a9b9de290bad793c859e10cdebb0a0a27d12
Anne Stellingwerf <astellingwerf@cordys.com>
2013-12-15 17:09:38 +0100

    Added a property to only compare SVT differences for a single team.

--- src/main/groovy/com/opentext/dropmerge/dsl/TestJobComparison.groovy
+++ src/main/groovy/com/opentext/dropmerge/dsl/TestJobComparison.groovy
@@ -6,6 +6,7 @@ import groovy.xml.MarkupBuilder
 
 class TestJobComparison {
     WikiTableBuilder table;
+    Properties props = loadProperties()
 
     static TestJobComparison compare(@DelegatesTo(TestJobComparison) Closure closure) {
         Writer writer = new StringWriter()
@@ -22,6 +23,9 @@ class TestJobComparison {
     }
 
     def forTeam(String teamName, @DelegatesTo(ComparingJobsSpec) jobsClosure) {
+        if (props.compareTeam && props.compareTeam != teamName)
+            return
+
         ComparingJobsSpec jobsSpec = new ComparingJobsSpec()
         jobsSpec.with jobsClosure
 
@@ -35,5 +39,25 @@ class TestJobComparison {
                 )
             }
         }
+    }
+
+    private Properties loadProperties() {
+        def p = new Properties()
+        def propertiesFromFile = new Properties()
+        ['team.properties', 'user.properties', 'session.properties'].each { String it ->
+            File f1 = new File(it)
+            if (f1.exists()) propertiesFromFile.load(f1.newInputStream())
+        }
+
+        final String propPrefix = this.class.simpleName
+        (System.getenv() + propertiesFromFile + System.getProperties()).each { props ->
+            props.each { prop ->
+                ['.', '_'].each { sep ->
+                    if (prop.key.startsWith(propPrefix + sep))
+                        p[prop.key[propPrefix.length() + 1..-1]] = prop.value
+                }
+            }
+        }
+        return p
     }
 }
