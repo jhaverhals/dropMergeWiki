c2689a8b89a17864bfb98cd2d3c42e997e8ed5ad
Anne Stellingwerf <astellingwerf@cordys.com>
2013-12-27 20:06:19 +0100

    Added support for more advanced comments of QualityAndProcess questions.

--- src/main/groovy/com/opentext/dropmerge/dsl/QualityAndProcessQuestionsSpec.groovy
+++ src/main/groovy/com/opentext/dropmerge/dsl/QualityAndProcessQuestionsSpec.groovy
@@ -11,83 +11,152 @@ class QualityAndProcessQuestionsSpec {
         this.inputs = inputs
     }
 
-    public void performanceDegraded(ComboBoxAnswers answer, String comment = null) {
+    public void performanceDegraded(ComboBoxAnswers answer, @DelegatesTo(FreeTextSpec) Closure comment = null) {
         addInput('PerformanceDegradation', [yes, no, notTested], answer, comment)
     }
 
-    public void xmlMemoryManagementIssuesIntroduced(ComboBoxAnswers answer, String comment = null) {
+    public void performanceDegraded(ComboBoxAnswers answer, String comment) {
+        performanceDegraded(answer, { withText comment })
+    }
+
+    public void xmlMemoryManagementIssuesIntroduced(ComboBoxAnswers answer,
+                                                    @DelegatesTo(FreeTextSpec) Closure comment = null) {
         addInput('MemoryLeaksIntroduced', [yes, no, notTested], answer, comment)
     }
 
-    public void regressionTestPassWithPayloadValidation(ComboBoxAnswers answer, String comment = null) {
+    public void xmlMemoryManagementIssuesIntroduced(ComboBoxAnswers answer, String comment) {
+        xmlMemoryManagementIssuesIntroduced(answer, { withText comment })
+    }
+
+    public void regressionTestPassWithPayloadValidation(ComboBoxAnswers answer,
+                                                        @DelegatesTo(FreeTextSpec) Closure comment = null) {
         addInput('RegressionTestsPassWithPayloadValidation', [yes, no, notTested], answer, comment)
     }
 
-    public void compliantWithHorizontalComponentRequirements(ComboBoxAnswers answer, String comment = null) {
+    public void regressionTestPassWithPayloadValidation(ComboBoxAnswers answer, String comment) {
+        regressionTestPassWithPayloadValidation(answer, { withText comment })
+    }
+
+    public void compliantWithHorizontalComponentRequirements(ComboBoxAnswers answer,
+                                                             @DelegatesTo(FreeTextSpec) Closure comment = null) {
         addInput('CompliantWithHorizontalComponentRequirements', [yes, no, partially], answer, comment)
     }
 
-    public void documentationReviewed(ComboBoxAnswers answer, String comment = null) {
+    public void compliantWithHorizontalComponentRequirements(ComboBoxAnswers answer, String comment) {
+        compliantWithHorizontalComponentRequirements(answer, { withText comment })
+    }
+
+    public void documentationReviewed(ComboBoxAnswers answer, @DelegatesTo(FreeTextSpec) Closure comment = null) {
         addInput('DocumentationReviewed', [yes, no, notApplicable], answer, comment)
     }
 
-    public void messagesTranslatable(ComboBoxAnswers answer, String comment = null) {
+    public void documentationReviewed(ComboBoxAnswers answer, String comment) {
+        documentationReviewed(answer, { withText comment })
+    }
+
+    public void messagesTranslatable(ComboBoxAnswers answer, @DelegatesTo(FreeTextSpec) Closure comment = null) {
         addInput('TranslatableMessages', [yes, no, notApplicable], answer, comment)
     }
 
-    public void defectFixesRetestedByOtherPerson(ComboBoxAnswers answer, String comment = null) {
+    public void messagesTranslatable(ComboBoxAnswers answer, String comment) {
+        messagesTranslatable(answer, { withText comment })
+    }
+
+    public void defectFixesRetestedByOtherPerson(ComboBoxAnswers answer,
+                                                 @DelegatesTo(FreeTextSpec) Closure comment = null) {
         addInput('DefectFixesRetestedByOtherPerson', [yes, no, notApplicable], answer, comment)
     }
 
-    public void alertsDocumented(ComboBoxAnswers answer, String comment = null) {
+    public void defectFixesRetestedByOtherPerson(ComboBoxAnswers answer, String comment) {
+        defectFixesRetestedByOtherPerson(answer, { withText comment })
+    }
+
+    public void alertsDocumented(ComboBoxAnswers answer, @DelegatesTo(FreeTextSpec) Closure comment = null) {
         addInput('DocumentedAlerts', [yes, no, notApplicable], answer, comment)
     }
 
-    public void multiPlatformValidationDone(ComboBoxAnswers answer, String comment = null) {
+    public void alertsDocumented(ComboBoxAnswers answer, String comment) {
+        alertsDocumented(answer, { withText comment })
+    }
+
+    public void multiPlatformValidationDone(ComboBoxAnswers answer, @DelegatesTo(FreeTextSpec) Closure comment = null) {
         addInput('MultiplatformValidationDone', [yes, no, notApplicable], answer, comment)
     }
 
-    def forwardPortingCompleted(ComboBoxAnswers answer, String comment = null) {
+    public void multiPlatformValidationDone(ComboBoxAnswers answer, String comment) {
+        multiPlatformValidationDone(answer, { withText comment })
+    }
+
+    public void forwardPortingCompleted(ComboBoxAnswers answer, @DelegatesTo(FreeTextSpec) Closure comment = null) {
         addInput('ForwardPortingCompleted', [yes, no, notApplicable], answer, comment)
     }
 
-    def migrationAspectsHandled(ComboBoxAnswers answer, String comment = null) {
+    public void forwardPortingCompleted(ComboBoxAnswers answer, String comment) {
+        forwardPortingCompleted(answer, { withText comment })
+    }
+
+    public void migrationAspectsHandled(ComboBoxAnswers answer, @DelegatesTo(FreeTextSpec) Closure comment = null) {
         addInput('MigrationAspectsHandled', [yes, no, notApplicable], answer, comment)
     }
 
-    def backwardCompatibilityIssuesIntroduced(ComboBoxAnswers answer, String comment = null) {
+    public void migrationAspectsHandled(ComboBoxAnswers answer, String comment) {
+        migrationAspectsHandled(answer, { withText comment })
+    }
+
+    public void backwardCompatibilityIssuesIntroduced(ComboBoxAnswers answer,
+                                                      @DelegatesTo(FreeTextSpec) Closure comment = null) {
         addInput('BackwardCompatibilityIssues', [yes, no], answer, comment)
     }
 
-    def securityIssuesIntroduced(ComboBoxAnswers answer, String comment = null) {
+    public void backwardCompatibilityIssuesIntroduced(ComboBoxAnswers answer, String comment) {
+        backwardCompatibilityIssuesIntroduced(answer, { withText comment })
+    }
+
+    public void securityIssuesIntroduced(ComboBoxAnswers answer, @DelegatesTo(FreeTextSpec) Closure comment = null) {
         addInput('SecurityIssuesIntroduced', [yes, no, notTested], answer, comment)
     }
 
-    def buildAndInstallerChangesAddressed(ComboBoxAnswers answer, String comment = null) {
+    public void securityIssuesIntroduced(ComboBoxAnswers answer, String comment) {
+        securityIssuesIntroduced(answer, { withText comment })
+    }
+
+    public void buildAndInstallerChangesAddressed(ComboBoxAnswers answer,
+                                                  @DelegatesTo(FreeTextSpec) Closure comment = null) {
         addInput('BuildAndInstallerChangesAddressed', [yes, no, notApplicable], answer, comment)
     }
 
-    def newManualTestCasesAdded(ComboBoxAnswers answer, String comment = null) {
-        newManualTestCasesAdded(answer.optionText, comment)
+    public void buildAndInstallerChangesAddressed(ComboBoxAnswers answer, String comment) {
+        buildAndInstallerChangesAddressed(answer, { withText comment })
     }
 
-    def newManualTestCasesAdded(String answer, String comment = null) {
+    public void newManualTestCasesAdded(ComboBoxAnswers answer, String comment) {
+        newManualTestCasesAdded(answer.optionText, { withText comment })
+    }
+
+    public void newManualTestCasesAdded(String answer, @DelegatesTo(FreeTextSpec) Closure comment = null) {
         addInput('NewManualTestCases', answer, comment)
     }
 
-    private void addInput(String fieldName, List<ComboBoxAnswers> allowed, ComboBoxAnswers answer, String comment) {
+    private void addInput(String fieldName, List<ComboBoxAnswers> allowed, ComboBoxAnswers answer,
+                          @DelegatesTo(FreeTextSpec) Closure comment) {
         if (!allowed.contains(answer))
             throw new IllegalArgumentException("answer for $fieldName must be one of these: $allowed")
 
         inputs[fieldName] = { item -> CordysWiki.selectOption(item, answer.optionText) }
-        if (comment)
-            inputs[fieldName + 'Comment'] = { comment }
+        if (comment) {
+            FreeTextSpec freeTextSpec = new FreeTextSpec()
+            freeTextSpec.with comment
+            inputs[fieldName + 'Comment'] = { freeTextSpec.text }
+        }
     }
 
-    private void addInput(String fieldName, String answer, String comment) {
+    private void addInput(String fieldName, String answer, @DelegatesTo(FreeTextSpec) Closure comment) {
         inputs[fieldName] = { answer }
-        if (comment)
-            inputs[fieldName + 'Comment'] = { comment }
+        if (comment) {
+            FreeTextSpec freeTextSpec = new FreeTextSpec()
+            freeTextSpec.with comment
+            inputs[fieldName + 'Comment'] = { freeTextSpec.text }
+        }
     }
 
     enum ComboBoxAnswers implements AnswerEnum {
