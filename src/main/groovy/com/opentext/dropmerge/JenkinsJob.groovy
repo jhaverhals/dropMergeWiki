jhaverhals/dropMergeWiki

commit 04ebf800c9c2bd5275f53d9a0e572c9f369d25a2

Several minor code optimizations.

src/main/groovy/com/opentext/dropmerge/JenkinsJob.groovy
@@ -17,8 +17,9 @@ class JenkinsJob {
         this.matrixAxes = matrixAxes
 
         if (matrixAxes) {
-            List<String[]> x = jsonForJob(null, null, 'activeConfigurations[name]')['activeConfigurations'].collect { it.name.split(',') }
-            def matches = x.findAll { String[] configuration ->
+            List<String[]> matches = jsonForJob(null, null, 'activeConfigurations[name]')['activeConfigurations']
+                    .collect { it.name.split(',') }
+                    .findAll { String[] configuration ->
                 matrixAxes.every { String axis, String value ->
                     configuration.contains("$axis=$value")
                 }
@@ -97,9 +98,9 @@ class JenkinsJob {
 
     @Memoized
     private def jsonForJob(String build, String subPage, String jsonPath, Integer depth = null) {
-        String url = [getBuildUrl(build), subPage, 'api','json'].findAll {it != null }.join('/')
-        if(jsonPath)    url +="?tree=$jsonPath"
-        else if(depth)  url += "?depth=$depth"
+        String url = [getBuildUrl(build), subPage, 'api', 'json'].findAll { it != null }.join('/')
+        if (jsonPath) url += "?tree=$jsonPath"
+        else if (depth) url += "?depth=$depth"
 
         return new JsonSlurper().parseText(new URL(url).text)
     }
@@ -124,7 +125,7 @@ class JenkinsJob {
     }
 
     public def getMatrixSubJobs() {
-        return jsonForJob(null, null, "activeConfigurations[name]")["activeConfigurations"].collect {
+        return jsonForJob(null, null, 'activeConfigurations[name]')['activeConfigurations'].collect {
             onInstance.withJob("$name/${it.name}")
         }
     }
@@ -137,10 +138,10 @@ class JenkinsJob {
     public java.lang.String toString() {
         String n
         if (matrixAxes) {
-            n = "'" + name.substring(0, name.indexOf('/')) + "' with $matrixAxes"
+            n = "'${name.takeWhile { it != '/' }}' with $matrixAxes"
         } else {
             n = "'$name'"
         }
-        return n + ' on ' + new URL(onInstance.rootUrl).host.replaceFirst('.vanenburg.com$', '');
+        return "$n on " + new URL(onInstance.rootUrl).host - ~/\.vanenburg\.com$/;
     }
 }

src/main/groovy/com/opentext/dropmerge/dsl/DateDsl.groovy
@@ -1,10 +1,12 @@
 package com.opentext.dropmerge.dsl
 
+import static java.util.Calendar.*
+
 class DateDsl {
     private Calendar today = Calendar.getInstance()
     private Boolean nextOdd = null
     private boolean includeToday = true
-    private int day = today.get(Calendar.DAY_OF_WEEK);
+    private int day = today[DAY_OF_WEEK]
 
     public DateDsl setIncludeToday(boolean b) {
         includeToday = b
@@ -67,17 +69,17 @@ class DateDsl {
     }
 
     Date getDate() {
-        int dayOfWeek = today.get(Calendar.DAY_OF_WEEK);
+        int dayOfWeek = today.get(DAY_OF_WEEK);
         int daysUntilNextTargetDOW = day - dayOfWeek
         if (daysUntilNextTargetDOW < 0 || (daysUntilNextTargetDOW == 0 && !includeToday)) {
             daysUntilNextTargetDOW = daysUntilNextTargetDOW + 7
         }
 
         Calendar nextTargetDOW = (Calendar) today.clone()
-        nextTargetDOW.add(Calendar.DAY_OF_WEEK, daysUntilNextTargetDOW)
+        nextTargetDOW.add(DAY_OF_WEEK, daysUntilNextTargetDOW)
         if (nextOdd != null) {
-            if (nextTargetDOW.get(Calendar.WEEK_OF_YEAR) % 2 != (nextOdd.booleanValue() ? 1 : 0)) {
-                nextTargetDOW.add(Calendar.DAY_OF_WEEK, 7)
+            if (nextTargetDOW[WEEK_OF_YEAR] % 2 != (nextOdd.booleanValue() ? 1 : 0)) {
+                nextTargetDOW.add(DAY_OF_WEEK, 7)
             }
         }
 

src/main/groovy/com/opentext/dropmerge/dsl/TeamSpec.groovy
@@ -7,7 +7,7 @@ import com.opentext.dropmerge.TransformerProvider
 class TeamSpec {
 
     private Map<String, Closure<String>> inputs
-    private List<String> userNames = []
+    private Set<String> userNames = []
 
     TeamSpec(Map<String, Closure<String>> inputs) {
         this.inputs = inputs

commit da599c8af91d75984266c4826e0989f62d941d63

Fixed something that I broke in 04ebf80

src/main/groovy/com/opentext/dropmerge/JenkinsJob.groovy
@@ -142,6 +142,6 @@ class JenkinsJob {
         } else {
             n = "'$name'"
         }
-        return "$n on " + new URL(onInstance.rootUrl).host - ~/\.vanenburg\.com$/;
+        return "$n on " + (new URL(onInstance.rootUrl).host - ~/\.vanenburg\.com$/);
     }
 }