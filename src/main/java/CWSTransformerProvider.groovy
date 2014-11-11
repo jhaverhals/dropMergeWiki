185d15b3ac2484d2d0cf46fbd8d2d5d0df6f3523
jhaverhals <johanhaverhals@gmail.com>
2014-11-11 11:53:38 +0100

    Update CWS transformation provider

--- src/main/java/CWSTransformerProvider.groovy
+++ src/main/java/CWSTransformerProvider.groovy
@@ -1,3 +1,4 @@
+import com.opentext.dropmerge.Jenkins
 import com.opentext.dropmerge.dsl.DropMergeInput
 
 class CWSTransformerProvider {
@@ -7,9 +8,16 @@ class CWSTransformerProvider {
             team {
                 name 'CWS'
                 productManager 'hkastenb'
-                architect 'rprins', 'awisse'
+                architect 'rprins', 'awisse', 'gjlubber'
                 scrumMaster 'rprins'
             }
+            
+		    dropMergeOn every.even.monday //.includingToday
+		
+		    functionalDescription {
+//		        withJiraIssuesTable "sprint = '${myProperties['sprintName']}' AND resolution = Fixed AND issuetype not in ('Bug during story', Todo)"
+		        withJiraIssuesTable "component='CWS team' AND resolution = Fixed AND issuetype not in ('Bug during story', Todo) and status in (Delivered, Done) and resolutionDate>'${myProperties['wipStartDate']}' and resolutionDate<='${myProperties['wipEndDate']}'"
+		    }
 
             wiki {
                 userName myProperties['wikiUserName']
@@ -25,6 +33,7 @@ class CWSTransformerProvider {
 
             jenkins {
                 regressionTests {
+                    Jenkins globalUIUnits = new Jenkins('http://10.192.69.9:8080')
                     ofType('JUnit') {
                         withJob { job 'cws-wip-junit-l' on buildMasterNL; description 'Linux' }
                         comparedToJob { job 'CWS-L' on jenkinsOfSVT; description 'Linux' }
@@ -36,45 +45,87 @@ class CWSTransformerProvider {
                         andJob { job 'CWSOldBuild-W' on jenkinsOfSVT; description 'Windows' }
                         andJob { job 'CAP-CWS-W' on jenkinsOfSVT; description 'Windows' }
                     }
-                    ofType('CWS UIUnits') {
+                    ofType('UIUnits') {
+                    
                         withJob {
-                            on buildMasterNL job 'cws-wip-uiunit' matrixValues browser: 'Chrome';
+                            job 'cws-wip-uiunit' on buildMasterNL matrixValues browser: 'Chrome';
                             description 'Chrome'
                         }
+                        comparedToJob{ job 'CWS-Trunk' on globalUIUnits}
                         withJob {
                             on buildMasterNL job 'cws-wip-uiunit' matrixValues browser: 'Firefox';
                             description 'Firefox'
                         }
-                    }
-                    ofType('GMF UIUnits Runtime Ref') {
+                        comparedToJob{ job 'CWS-Trunk' on globalUIUnits}
+                     }
+                    ofType('UIUnits Runtime Ref') {
                         withJob {
-                            on buildMasterNL job 'cws-wip-uiunit-runtime-ref-test' matrixValues browser: 'Chrome';
+                           job 'cws-wip-uiunit-runtime-ref-test' on buildMasterNL  matrixValues browser: 'Chrome';
                             description 'Chrome'
                         }
+                        comparedToJob{ job 'CWS-Runtime-Trunk' on globalUIUnits}
                         withJob {
-                            on buildMasterNL job 'cws-wip-uiunit-runtime-ref-test' matrixValues browser: 'Firefox';
+                            job 'cws-wip-uiunit-runtime-ref-test' on buildMasterNL matrixValues browser: 'Firefox';
                             description 'Firefox'
                         }
+                        comparedToJob{ job 'CWS-Runtime-Trunk' on globalUIUnits}
                     }
-                    ofType('GMF UIUnits') {
+                    ofType('GMF') {
                         withJob { on buildMasterNL job 'cws-wip-gmf-chrome'; description 'Chrome' }
+                        comparedToJob{job 'GraphicalModelingFramework-Trunk' on globalUIUnits }
                         withJob { on buildMasterNL job 'cws-wip-gmf-ff'; description 'Firefox' }
+                        comparedToJob{job 'GraphicalModelingFramework-Trunk' on globalUIUnits }
                     }
                 }
                 upgrade {
-                    withJob { on buildMasterNL job 'A_UP_4.1CU7.1-WIP_W'; description 'on Windows from BOP 4.1 CU7.1' }
-                    withJob { on buildMasterNL job 'B_UP_4.1CU6-4.1CU7.1_L'; description 'on Linux from BOP 4.1 CU6 via BOP 4.1 CU7.1' }
-                    withJob { on buildMasterNL job 'C_UP_4.1CU7.1-WIP_L'; description 'on Linux from BOP 4.1 CU7.1' }
+                    withJob { on buildMasterNL job 'A_UP_4.1CU7.1-WIP_W'; description 'on Windows from BOP 4.1 CU7.1 to WIP' }
+                    withJob { on buildMasterNL job 'B_UP_4.1CU6-4.1CU7.1_L'; description 'on Linux from BOP 4.1 CU6 to BOP 4.1 CU7.1' }
+                    withJob { on buildMasterNL job 'C_UP_4.1CU7.1-WIP_L'; description 'on Linux from BOP 4.1 CU7.1 to WIP' }
                     withJob { on buildMasterNL job 'D_UP_4.2CU1-WIP_L'; description 'on Linux from BOP 4.2 CU1' }
                     withJob { on buildMasterNL job 'E_UP_4.1CU7.1-WIP_W'; description 'on Windows from BOP 4.1 CU7.1' }
                     withJob { on buildMasterNL job 'F_UP_4.2CU1-WIP_W'; description 'on Windows from BOP 4.2 CU1' }
-                    withJob { on buildMasterNL job 'wip-content-upgrade'; description 'to test content migration' }
+                    withJob { on buildMasterNL job 'wip-content-upgrade'; description 'on Windows from BOP4.1 CU7.1 to WIP with design-time content' }
                 }
+                pmd {
+		            trunk { job 'cws-wip-metrics' on buildMasterNL }
+		            wip { job 'cws-wip-metrics' on buildMasterNL }
+		        }
+		        compilerWarnings {
+		            trunk { job 'cws-wip-metrics' on buildMasterNL }
+		            wip { job 'cws-wip-metrics' on buildMasterNL }
+		        }
+		        mbv {
+		            trunk { job 'cws-wip-metrics' on buildMasterNL }
+		            wip { job 'cws-wip-metrics' on buildMasterNL }
+		        }
                 integrationTests {
-                    withJob { job 'cws-wip-uiunit-EW-NewBuildEngine' on buildMasterNL }
-                    withJob { job 'cws-wip-smoketest' on buildMasterNL }
+                    withJob { job 'cws-wip-uiunit-EW-NewBuildEngine' on buildMasterNL; description 'Eastwind' }
+                    withJob { job 'cws-wip-smoketest' on buildMasterNL; description 'Smoke Test'}
                 }
             }
+        qualityAndProcessQuestions {
+        	xmlMemoryManagementIssuesIntroduced no, 'INU detection enabled.'
+        	regressionTestsPassWithPayloadValidation yes
+        	compliantWithHorizontalComponentRequirements yes
+        	documentationReviewed yes, 'Embedded in the development process.'
+        	defectFixesRetestedByOtherPerson yes
+        	alertsDocumented notApplicable
+        	multiPlatformValidationDone yes
+        	buildAndInstallerChangesAddressed notApplicable
+        	messagesTranslatable yes, 'Embedded in the development process.'
+        	newManualTestCasesAdded no, 'No new manual tests added. We prefer automated tests.'
+        	forwardPortingCompleted yes, 'We always first fix in our own WIP.'
+        	securityIssuesIntroduced no, 'Guarded by code reviews.'
+        	migrationAspectsHandled yes
+        	performanceDegraded no
+					backwardCompatibilityIssuesIntroduced no
+        	/*{
+        		withHtml JenkinsSpec.getJenkinsUrlWithStatus(new Jenkins('http://buildmaster-nl/jenkins').withJob('FP1-LOADTEST-MYSQL'))
+        	}*/
         }
+        
+     }
+        
+
     }
 }
